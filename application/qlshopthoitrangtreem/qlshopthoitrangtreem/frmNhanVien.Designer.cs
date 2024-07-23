4584ce76a184cf7cdb34d5c792fb15fa663cde96
trinhchitai <tiemney@gmail.com>
2024-07-23 13:35:17 +0700

    add file

--- application/qlshopthoitrangtreem/qlshopthoitrangtreem/frmNhanVien.Designer.cs
+++ application/qlshopthoitrangtreem/qlshopthoitrangtreem/frmNhanVien.Designer.cs
@@ -29,19 +29,31 @@ namespace qlshopthoitrangtreem
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(636, 112);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 0;
             // 
             // frmNhanVien
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1088, 450);
+            this.Controls.Add(this.textBox1);
             this.Name = "frmNhanVien";
             this.Text = "frmNhanVien";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
